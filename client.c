4ba47d88c9d37b1785c1f8fab10d45c80b0a08c0
Radu Ioan Fericean <radu@fericean.ro>
2013-08-15 16:31:25 +0300

    modular call

--- client.c
+++ client.c
@@ -8,48 +8,60 @@
 #define SERVER_ADDR "127.0.0.1"
 #define PORTNUM 1234
 
-void error(const char *msg)
-{
+int id = 1;
+
+void error(const char *msg) {
     perror(msg);
     exit(0);
 }
 
-int main(int argc, char *argv[])
-{
-    int sockfd, n;
-    struct sockaddr_in serv_addr;
+int initSocket(const char *addr, const int port){
+  int sockfd;
+  struct sockaddr_in serv_addr;
+  // socket init and connect
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) 
+    error("ERROR opening socket");
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr(addr);
+  serv_addr.sin_port = htons(port);
+  
+  if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+    error("ERROR connecting");
+  return sockfd;
+}
 
-    char buffer[MAXRCVLEN];
-    char msg[] = "{\"params\": [{\"A\": 2, \"B\": 3}], \"id\": 1, \"method\": \"Arith.Multiply\"}";
-    
-    // socket init and connect
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
-    serv_addr.sin_port = htons(PORTNUM);
- 
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
+void rpcCall(const int sockfd, const char *method, const char *params, char *buf) {
+  int n;
+  char msg[MAXRCVLEN];
+  sprintf(msg, "{\"id\": %d, \"method\": \"%s\", \"params\": [%s]}", id++, method, params);
+  // writing the call
+  n = write(sockfd, msg, strlen(msg));
+  
+  if (n < 0) 
+    error("ERROR writing to socket");
 
-    // writing the call
-    n = write(sockfd, msg, strlen(msg));
-    
-    if (n < 0) 
-         error("ERROR writing to socket");
+  // reading the response
+  bzero(buf,MAXRCVLEN);
+  n = read(sockfd, buf, MAXRCVLEN);
+  
+  if (n < 0) 
+    error("ERROR reading from socket");
+}
 
-    // reading the response
-    bzero(buffer,MAXRCVLEN);
-    n = read(sockfd, buffer, MAXRCVLEN);
-    
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+int main(int argc, char *argv[]) {
+  int sockfd;
+  char response[MAXRCVLEN];
+  
+  sockfd = initSocket(SERVER_ADDR, PORTNUM);
+  
+  rpcCall(sockfd, "Arith.Multiply", "{\"A\": 2, \"B\": 3}", response);
 
-    close(sockfd);
-    return 0;
+  printf("%s\n", response);
+  
+  close(sockfd);
+  return 0;
 }
 
 
