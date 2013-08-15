785371d18aeca4f8d78586f52265edab40c28881
Radu Ioan Fericean <radu@fericean.ro>
2013-08-15 15:01:57 +0300

    sending only working version

--- client.c
+++ client.c
@@ -1,12 +1,12 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h> 
+#include <arpa/inet.h>
+
+#define MAXRCVLEN 4096
+#define SERVER_ADDR "127.0.0.1"
+#define PORTNUM 1234
 
 void error(const char *msg)
 {
@@ -16,43 +16,40 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
+    int sockfd, n;
     struct sockaddr_in serv_addr;
-    struct hostent *server;
 
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
+    char buffer[MAXRCVLEN];
+    char msg[] = "{\"params\": [{\"A\": 2, \"B\": 3}], \"id\": 1, \"method\": \"Arith.Multiply\"}";
+    
+    // socket init and connect
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
+    serv_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+    serv_addr.sin_port = htons(PORTNUM);
+ 
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
+
+    // writing the call
+    n = write(sockfd, msg, strlen(msg));
+    
     if (n < 0) 
          error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
+
+    // reading the response
+    bzero(buffer,MAXRCVLEN);
+    n = read(sockfd, buffer, MAXRCVLEN);
+    
     if (n < 0) 
          error("ERROR reading from socket");
     printf("%s\n",buffer);
+
     close(sockfd);
     return 0;
 }
+
+
